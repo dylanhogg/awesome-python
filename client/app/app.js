04971a7900304f00098cc18a959b745c4e3a4515
dylan <dylanhogg@gmail.com>
2021-04-12 12:24:17 +1000

    Update client app.

--- client/app/app.js
+++ client/app/app.js
@@ -7,13 +7,24 @@ $(document).ready( function () {
             url: 'http://prd-s3-crazy-awesome-python-api.s3-website-us-east-1.amazonaws.com/github_data.json',
             dataSrc: 'data'
         },
+        order: [[ 4, "desc" ]],
         columns: [
-          { data: "category", title: "category" },
-          { data: "_description", title: "description" },
-          { data: "githuburl", title: "url" },
-          { data: "_stars", title: "stars" },
-          { data: "_stars_per_week", title: "stars/wk" },
-          { data: "_forks", title: "forks" },
+          { data: "category", title: "Category" },
+          { data: "_description", title: "Description" },
+          { data: "_repopath", title: "Github",
+            render: function(data, type, row, meta) { return "<a href='https://github.com/" + data + "'>" + data + "</a>"; }
+          },
+          { data: "_stars", title: "Stars", render: $.fn.dataTable.render.number(',', '.', 0) },
+          { data: "_stars_per_week", title: "Stars\nper&nbsp;week",
+            render: function(data, type, row, meta) { return data > 10 ? data.toFixed(0) : data.toFixed(1); }
+          },
+          { data: "_forks", title: "Forks", render: $.fn.dataTable.render.number(',', '.', 0) },
+          { data: "_created_at", title: "Created",
+            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
+          },
+          { data: "_age_weeks", title: "Age in&nbsp;weeks",
+            render: function(data, type, row, meta) { return data.toFixed(0); }
+          },
         ],
         paging: false,
     });
