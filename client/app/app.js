941133fdf0eed10ffcd9bd03c653559435888f73
Dylan Hogg <dylanhogg@gmail.com>
2023-03-07 16:45:23 +1100

    Category and topics with tag UI and qs enhancements.

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,5 @@
 const version = "v0.0.10";
-const CATEGORY_COL = 7;
+const CATEGORY_COL = 7;  // 0-based
 const TAG_COL = 8;
 
 function getUrlParams() {
@@ -23,6 +23,22 @@ function getUrlQuery() {
     }
 }
 
+function deleteUrlQueryKey(paramKey) {
+    try {
+        var href = new URL(location.href);
+        href.searchParams.delete(paramKey);
+        window.history.pushState("", "unused", href);
+    } catch(err) { }
+}
+
+function setUrlQueryValue(paramKey, paramVal) {
+    try {
+        var href = new URL(location.href);
+        href.searchParams.set(paramKey, paramVal);
+        window.history.pushState("", "unused", href);
+    } catch(err) { }
+}
+
 function getUrlCategoryFilter() {
     try {
         var params = getUrlParams();
@@ -33,9 +49,9 @@ function getUrlCategoryFilter() {
 }
 
 $(document).keydown(function(e) {
-    if (e.keyCode == 39) {  // Right arrow
+    if (e.keyCode == 39 || e.keyCode == 34) {  // Right arrow or page down
         $("#table").DataTable().page("next").draw("page");
-    } else if (e.keyCode == 37) {  // Left arrow
+    } else if (e.keyCode == 37 || e.keyCode == 33) {  // Left arrow or page up
         $("#table").DataTable().page("previous").draw("page");
     } else if (e.keyCode == 27) {  // Escape
         $('#sidenav').width("0px");
@@ -45,7 +61,8 @@ $(document).keydown(function(e) {
 // Category filter dropdown
 $(document).on("preInit.dt", function (e, settings) {
     var data = {
-        '': 'All Categories',
+        '': 'Select category...',
+        'all': 'All categories',
         'crypto': 'Crypto',
         'data': 'Data',
         'debug': 'Debugging',
@@ -84,19 +101,22 @@ $(document).on("preInit.dt", function (e, settings) {
     select.appendTo('div.dataTables_filter');
 
     select.change(function(){
-        category_filter = $("#category_filter").val()
+        category_filter = $("#category_filter").val();
         var table = $("#table").DataTable();
-        if (category_filter == "") {
+        if (category_filter == "" || category_filter == "all") {
+            deleteUrlQueryKey("c");
             table
                 //.search("")  // Clear full-table search
                 .columns(CATEGORY_COL)
                 .search("")
                 .draw();
         } else {
+            setUrlQueryValue("c", category_filter);
             table
                 // .search("")  // Clear full-table search
                 .columns(CATEGORY_COL)
-                .search("^"+category_filter+"$", true, false)  // regex search
+                //.search(category_filter, true, false)  // too broad, e.g. ml gets ml-dl also
+                .search("^"+category_filter+"$", true, false)  // regex search over plain text (ensure filter: true for <a>...</a> columns)
                 .draw();
         }
     });
@@ -155,28 +175,35 @@ $(document).ready( function () {
     });
 
     var initialSearchTerm = getUrlQuery();
-    var description_maxlen = 180;
+    var description_max_strlen = 100;
+    var topic_max_strlen = 30;
+    var topic_max_count = 3;
     var table = $("#table").DataTable( {
         ajax: {
             url: ajax_url,
             dataSrc: 'data'
         },
+        // searchDelay: 100,
         deferRender: true,
         responsive: true,
         order: [[ 1, "desc" ]],
         paging: true,
+        pagingType: "full",  // https://datatables.net/reference/option/pagingType
         lengthChange: true,
         lengthMenu: [[5, 10, 50, -1], [5, 10, 50, "All"]],
-        pageLength: 10,
+        pageLength: 5,
         search: {
            search: initialSearchTerm,
         },
+        filter: true,  // Enables anchor wrapped category filtering
         language: {
-            searchPlaceholder: "Search",
+            searchPlaceholder: "Search...",
             search: "",
           },
         // dom: 'lfrtip',  // Default. https://datatables.net/reference/option/dom
-        dom: 'frtilp',
+        // dom: 'frtilp',  // Awesome Python - Classic
+        dom: 'frtpil',  // Awesome Python - Page above info, needs some custom css for alignment
+        // dom: '<"top"fp>rt<"bottom"il><"clear">',  // Awesome Python - Top Buttons
         columns: [
           { data: null,
             title: "Name",
@@ -190,6 +217,9 @@ $(document).ready( function () {
            { data: "_stars", title: "Stars&nbsp;⭐", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
            { data: "_stars_per_week", title: "Stars<br />per&nbsp;week",
             render: function(data, type, row, meta) { return data > 10 ? data.toFixed(0) : data.toFixed(1); }
+           },
+           { data: "_age_weeks", title: "Age in&nbsp;weeks",
+            render: function(data, type, row, meta) { return data.toFixed(0); }
            },
 
 //           { data: "_pop_contributor_count", title: "contributors", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
@@ -209,8 +239,8 @@ $(document).ready( function () {
 
            { data: "_description", title: "Description",
              render: function(data, type, row, meta) {
-                if(data.length > description_maxlen) {
-                    return "<div class='text-wrap description-column'>" + data.substr(0, description_maxlen) + "...</div>";
+                if(data.length > description_max_strlen) {
+                    return "<div class='text-wrap description-column'>" + data.substr(0, description_max_strlen) + "...</div>";
                 } else {
                     return "<div class='text-wrap description-column'>" + data + "</div>";
                 }
@@ -223,13 +253,10 @@ $(document).ready( function () {
                 var orgUrl = "<br /><a href='https://github.com/" + row._organization + "' target='_blank'>" + "<img src='img/org.png' width='16' height='16' alt='organisation' title='View GitHub organisation' class='github-img'></img></a>&nbsp;<a href='https://github.com/" + row._organization + "'>" + row._organization.toLowerCase() + "</a>";
                 var homepageUrl = "";
                 try { homepageUrl = "<br /><a href='" + row._homepage + "' target='_blank'><img src='img/web16.png' width='16' height='16' alt='homepage' title='View homepage' class='web-img'></img></a>&nbsp;<a href='" + row._homepage + "'>" + new URL(row._homepage).hostname + "</a>"; } catch { }
-                return repoUrl + orgUrl + homepageUrl;
+                return "<div class='text-wrap links-column'>" + repoUrl + orgUrl + homepageUrl + "</div>";
              }
            },
 //           { data: "_forks", title: "Forks&nbsp;<img src='img/fork.png' class='github-img' />", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
-           { data: "_age_weeks", title: "Age in&nbsp;weeks",
-            render: function(data, type, row, meta) { return data.toFixed(0); }
-           },
 //           { data: "_created_at", title: "Created&nbsp;<img src='img/clock.png' class='github-img' />",
 //            className: "text-nowrap",
 //            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
@@ -238,21 +265,40 @@ $(document).ready( function () {
 //            className: "text-nowrap",
 //            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
 //           },
-           { data: "category", title: "Category" },
-           { data: "_topics", title: "Tags",
-            render: function(data, type, row, meta) { return data.slice(0, 3).join(", "); }
+           { data: "category", title: "Category"
+             ,render: function(data, type, row, meta) {
+                // return data;
+                return "<a class='label-link' href='/app.html?c=" + data + "'>" + data + "</a>";
+             }
            },
-           { data: "_readme_localurl", title: "Docs",
-            orderable: false,
+           { data: "_topics", title: "Tags",
+            // render: function(data, type, row, meta) { return data.slice(0, 3).join(", "); }
             render: function(data, type, row, meta) {
-                if (data.length > 0) {
-                    var url = "/data/" + data + "";
-                    return "<img src='img/github16.png' width='16' height='16' alt='docs' title='View repo readme documentation' class='modal-ajax info-img' href='#' data-localurl='"+url+"' data-ext='.html' data-title='' data-replace-lf='false'></img>";
-                } else {
-                    return "";
-                }
+                if (data.length == 0) { return ""; }
+                var tags = data.slice(0, topic_max_count);
+                return tags.map(item => {
+                    var short_item = item;
+                    if (short_item.length > topic_max_strlen) {
+                        short_item = short_item.substr(0, topic_max_strlen);
+                    }
+                    // return "<a class='label-link' href='https://github.com/topics/" + item + "?l=python'>" + item + "</a> ";;
+                    return "<a class='label-link' href='/app.html?q=" + short_item + "'>" + short_item + "</a> ";
+                }).join(" ");
             }
            },
+
+//           { data: "_readme_localurl", title: "Docs",
+//            orderable: false,
+//            render: function(data, type, row, meta) {
+//                if (data.length > 0) {
+//                    var readme_localurl = "/data/" + data + "";
+//                    return "<img src='img/github16.png' width='16' height='16' alt='docs' title='View repo readme documentation' class='modal-ajax info-img' href='#' data-localurl='"+readme_localurl+"' data-ext='.html' data-title='' data-replace-lf='false'></img>";
+//                } else {
+//                    return "";
+//                }
+//            }
+//           },
+
 //          { data: "_requirements_localurls", title: "Requirements",
 //            render: function(data, type, row, meta) {
 //                if (data.length > 0) {
@@ -276,63 +322,45 @@ $(document).ready( function () {
 //                }
 //            }
 //          },
-        ],
+        ],  // end of columns
     });
 
-//    $("#category_filter").change(function(){
-//        category_filter = $("#category_filter").val()
-//        console.log("category_filter:" + category_filter);
-//        console.log(table);
+    table.on('search.dt', function () {
+        if (table.search() == "") {
+            // Reset "q" url param (if exists) on cleared search box
+            deleteUrlQueryKey("q");
+        }
+    });
+
+    // Model for displaying _readme_localurl pop-up docs (deprecated)
+//    $('#table').on('click', '.modal-ajax', function(e) {
+//        var localurl = $(this).data('localurl') + $(this).data('ext');
+//        e.preventDefault();
 //
-//        if (category_filter == "") {
-//            table
-//                //.search("")  // Clear full-table search
-//                .columns(CATEGORY_COL)
-//                .search("")
-//                .draw();
-//        } else {
-//            table
-//                // .search("")  // Clear full-table search
-//                .columns(CATEGORY_COL)
-//                .search("^"+category_filter+"$", true, false)  // regex search
-//                .draw();
-//        }
+//        $.ajax({
+//           type: "GET",
+//           url: localurl,
+//           title: $(this).data('title'),
+//           replace_lf: $(this).data('replace-lf'),
+//           success: function(content)
+//           {
+//                if (this.replace_lf) {
+//                    content = content.replace(/\n/g, '<br />');
+//                }
+//                var html = "<div class='modal'>";
+//                if (this.title.length > 0) {
+//                    html = html + "<b>" + this.title + "</b><br /><br />";
+//                }
+//                html = html + content + "</div>";
+//                $(html).appendTo("#container").modal();
+//           },
+//           error: function(html)
+//           {
+//                console.log("ERROR getting localurl: " + localurl);
+//           },
+//        });
+//        return false;
 //    });
-
-//    var initialCategoryFilter = getUrlCategoryFilter();
-//    if (initialCategoryFilter.length > 0) {
-//        $("#category_filter").val(initialCategoryFilter).change();
-//    }
-
-    $('#table').on('click', '.modal-ajax', function(e) {
-        var localurl = $(this).data('localurl') + $(this).data('ext');
-        e.preventDefault();
-
-        $.ajax({
-           type: "GET",
-           url: localurl,
-           title: $(this).data('title'),
-           replace_lf: $(this).data('replace-lf'),
-           success: function(content)
-           {
-                if (this.replace_lf) {
-                    content = content.replace(/\n/g, '<br />');
-                }
-                var html = "<div class='modal'>";
-                if (this.title.length > 0) {
-                    html = html + "<b>" + this.title + "</b><br /><br />";
-                }
-                html = html + content + "</div>";
-                $(html).appendTo("#container").modal();
-           },
-           error: function(html)
-           {
-                console.log("ERROR getting localurl: " + localurl);
-           },
-        });
-
-        return false;
-    });
 });
 
 
