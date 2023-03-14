8556fc522cf03589b1ac19e4a694a61de750f5ff
Dylan Hogg <dylanhogg@gmail.com>
2023-03-14 12:33:28 +1100

    Finalise best hot/top repos.

--- client/app/best.js
+++ client/app/best.js
@@ -56,12 +56,12 @@ $(document).ready( function () {
 
     var ajax_url = (window.location.href.indexOf("hot") !== -1) ? './github_hot.json?v1.0' : './github_top.json?v1.0';
     $.get(ajax_url, function (data) {
-        var tbl = $("<table />").attr("id", "top_repos_table");
-        $("#top_repos").append(tbl);
+        var tbl = $("<table />").attr("id", "best_repos_table");
+        $("#best_repos").append(tbl);
 
         var MAX_ASSETS = 3;
-        var html = "";
         var repos = data["data"];
+        var td_html = "";
         for(var i=0; i<repos.length; i++)
         {
             var row = repos[i];
@@ -76,23 +76,28 @@ $(document).ready( function () {
                 return a & a;
               }, 0);
 
-            var category_start = "";
+            // Row header
             if (rank == 1) {
-                html = "<tr><td>";
-                // var category_link = "<a class='label-link' title='" + CATEGORY_DATA[row.category] + "' href='/?c=" + row.category + "'>" + row.category + "</a>";
-                var category_link = "<h1><a class='label-link' title='" + CATEGORY_DATA[row.category] + "' href='/?c=" + row.category + "'>" + CATEGORY_DATA[row.category] + "</a></h1>";
-                html += category_link + "<br />";
+                var href_title = "View more repos in category " + CATEGORY_DATA[row.category];
+                header = "<tr><td colspan='3'>";
+                header += "<hr /><h4><a title='" + href_title + "' href='/?c=" + row.category + "'>" + CATEGORY_DATA[row.category] + "</a></h4>";
+                header += "</tr>";
+                td_html = "<tr>";
+                $("#best_repos_table").append(header);
             }
 
+            // Row cells
             var asset_title = row._repopath + ", Score: " + row._pop_score + ", Stars per week: " + row._stars_per_week;
             var asset_url = "https://opengraph.githubassets.com/" + hashCode + "/" + org_repo; // + "?size=50";
-            var asset_img = "<img src='" + asset_url + "' width='32%' alt='" + asset_title + "' />";
+            var asset_img = "<img src='" + asset_url + "' width='100%' alt='" + asset_title + "' />";
             var asset_link = "<a href='https://github.com/" + row._repopath + "' title='" + asset_title + "' alt='" + asset_title + "'>" + asset_img + "</a>";
-            html += asset_link + "&nbsp;";
+            td_html += "<td>" + asset_link + "</td>"
 
+            // Row footer
             if (rank == MAX_ASSETS) {
-                html + "</td></tr>";
-                $("#top_repos_table").append(html);
+                // if (i<7) { alert(rank); }
+                td_html += "</tr>";
+                $("#best_repos_table").append(td_html);
             }
         }
     });
